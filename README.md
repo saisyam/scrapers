2b78a2979b6d3de7da69d23e6c8330e8295554b6
Saisyam <saisyam@saisyam.com>
2021-03-11 14:38:47 +0530

    Added new scraper to scrape https://nykaa.com

--- README.md
+++ README.md
@@ -6,6 +6,11 @@ Scrapers built using Python Scrapy
 
 * [NYTimes](https://github.com/saisyam/scrapers/blob/main/news/news/spiders/nytimes_spider.py) scraper to extract latest news from sections like Politics, Business etc.
 
+## Skincare Products and Reviews Scrapper
+[Skincare scraper](https://github.com/saisyam/scrapers/tree/main/skincare) will scrape websites that sell various skincare products.
+
+* [Nykaa](https://github.com/saisyam/scrapers/blob/main/skincare/derma/spiders/nykaa_spider.py) scraper to extract product information and review URL. Currently hard coded for Laxme products page.
+
 # DISCLAIMER
 These scrapers are for educational or research purposes only. Any commerical use of this code is not entertained or recommended.
 
---
+++ skincare/derma/spiders/nykaa_spider.py
@@ -0,0 +1,34 @@
+import scrapy
+from scrapy.http.request import Request
+
+class NykaaSpider(scrapy.Spider):
+    name = "nykaa"
+
+    def start_requests(self):
+        urls = [
+            "https://www.nykaa.com/brands/lakme/c/604",
+        ]   
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+    
+    def parse_product(self, response):
+        title = response.css("div.product-des__details-title h1::text").get()
+        price_meta = response.css("div.price-info meta")
+        price = price_meta[0].attrib['content']
+        currency = price_meta[1].attrib['content']
+        url_items = response.request.url.split("/p/")
+        review_link = url_items[0]+"/reviews/"+url_items[1]+"?ptype=reviews&skuId="+url_items[1]
+        yield {
+            'url' : response.request.url,
+            'title': title,
+            'price': price,
+            'currency': currency,
+            'review_link': review_link,
+        }
+    def parse(self, response):
+        main_div = response.css("div.main-product-listing-page")
+        list_div = main_div.css("div.desktop")
+        for item in list_div.css("div.card-wrapper-container"):
+            atag = item.css("div.product-list-box a")
+            link = "https://nykaa.com"+atag.attrib['href']
+            yield Request(url = link, callback=self.parse_product, priority=1)
